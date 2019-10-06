e86aea9db0c3e94fec9bc0d730a32d0b232bef71
Arjun Cosare <arjun.cosare@outlook.com>
2019-10-06 14:03:58 -0600

    Back Up for 10/6/2019

---
+++ DisappearingUnicorns/SettingViewController.swift
@@ -0,0 +1,197 @@
+//
+//  SettingViewController.swift
+//  DisappearingUnicorns
+//
+//  Created by Arjun Cosare on 9/30/19.
+//  Copyright © 2019 Arjun cosare. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var playerPhoto: UIImageView!
+    @IBOutlet weak var playerAgeLabel: UILabel!
+    @IBOutlet weak var playerName: UITextField!
+    @IBOutlet weak var playerAge: UITextField!
+    @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var bgSwitchState: UISwitch!
+    @IBOutlet weak var colorSegment: UISegmentedControl!
+    @IBOutlet weak var gameSpeedLabel: UILabel!
+    @IBOutlet weak var gameSpeedSlider: UISlider!
+    @IBOutlet weak var backgroundCircle: UIButton!
+    
+    
+    var playerInfo: PlayerData?
+    let defaults = UserDefaults.standard
+    let gameData = GameData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let playerInfo = playerInfo{
+            playerPhoto.image = playerInfo.photo
+            playerName.text = playerInfo.name
+            playerAge.text = defaults.string(forKey: "playerAge") ?? ""
+            playerAgeLabel.text = "Age: \(defaults.string(forKey: "playerAge") ?? "None")"
+        }
+        configureTextFields()
+        roundedButton()
+        configureState()
+        // Do any additional setup after loading the view.
+    }
+    
+    func backgroundColorChange(){
+        if(bgSwitchState.isOn)
+        {
+            backgroundCircleChange(defaults.integer(forKey: "colorSegmentIndex"))
+            backgroundCircle.layer.borderWidth = 0
+        }
+        else
+        {
+            backgroundCircleChange(-1)
+            backgroundCircle.layer.borderWidth = 2
+            
+        }
+    }
+    
+    //Actions
+    @IBAction func updateButtonPressed(_ sender: Any) {
+        let updateAlert = UIAlertController(title: "Update Age, message", message: "Are you sure you want to update the player age to \(playerAge.text!)", preferredStyle: .alert)
+        
+        updateAlert.addAction(UIAlertAction(title: "Update", style: .default, handler: {
+            action in
+            
+            self.playerInfo?.name = self.playerName.text!
+            
+            self.defaults.set(self.playerName.text!, forKey: "playerName")
+            self.defaults.set(self.playerAge.text!, forKey: "playerAge")
+            
+            self.playerAgeLabel.text = "Age: \(self.playerAge.text!)"
+            
+            
+            }))
+        
+        updateAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel){(action) in
+            self.view.endEditing(true)
+        })
+        self.view.endEditing(true)
+        
+        self.present(updateAlert, animated: true)
+    }
+    
+    @IBAction func bgSwitchPressed(_ sender: Any) {
+        if(bgSwitchState.isOn)
+        {
+            defaults.set(true, forKey: "bgSwitchState")
+        }
+        else
+        {
+            defaults.set(false, forKey: "bgSwitchState")
+        }
+        backgroundColorChange()
+        
+    }
+    
+    @IBAction func colorSegmentPressed(_ sender: Any) {
+        defaults.set(colorSegment.selectedSegmentIndex, forKey: "colorSegmentIndex")
+        backgroundColorChange()
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let currentValue = Float(sender.value)
+        gameSpeedLabel.text = "\(String(format: "%.2f",currentValue))s / Round"
+        defaults.set(gameSpeedSlider.value, forKey: "gameSpeed")
+    }
+    
+    @IBAction func changePhotoPressed(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = true
+        
+        self.present(image, animated: true)
+        {
+            
+        }
+        
+        
+    }
+    
+    //configurables
+    private func configureTextFields(){
+        playerName.delegate = self
+        playerAge.delegate = self
+    }
+    
+    private func roundedButton(){
+        updateButton.layer.cornerRadius = 5
+        backgroundCircle.layer.cornerRadius = backgroundCircle.frame.width * 0.5
+        backgroundCircle.layer.borderWidth = 2
+        backgroundCircle.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    private func configureState()
+    {
+        //Set default on background switch
+        bgSwitchState.setOn(defaults.bool(forKey: "bgSwitchState"), animated: false)
+        
+        
+        //Set background color circle
+        backgroundColorChange()
+ 
+        //Set color last picked color segment
+        colorSegment.selectedSegmentIndex = defaults.integer(forKey: "colorSegmentIndex")
+        
+        //Set last set gamespeed
+        gameSpeedLabel.text = "\(defaults.float(forKey: "gameSpeed") == 0.0 ? "0.1" : String(format: "%.2f",defaults.float(forKey: "gameSpeed")))s / Round"
+        
+        //Slider config
+        gameSpeedSlider.minimumValue = 0.1
+        gameSpeedSlider.maximumValue = 5.0
+        gameSpeedSlider.setValue(defaults.float(forKey: "gameSpeed"), animated: false)
+        defaults.set(gameSpeedSlider.value, forKey: "gameSpeed")
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            gameData.updateProPic(for: self.playerInfo!.name, photo: image)
+            playerInfo?.photo = image
+            playerPhoto.image = image
+            
+        }
+        else
+        {
+        //error
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // Helpers
+    private func backgroundCircleChange(_ colorIndex: Int){
+        switch colorIndex {
+        case 0:
+            backgroundCircle.backgroundColor = UIColor.red
+        case 1:
+            backgroundCircle.backgroundColor = UIColor.blue
+        case 2:
+            backgroundCircle.backgroundColor = UIColor.green
+        case 3:
+            backgroundCircle.backgroundColor = UIColor.yellow
+        default:
+            backgroundCircle.backgroundColor = UIColor.white
+        }
+    }
+    
+
+}
+
+extension SettingViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ DisappearingUnicorns/ViewController.swift
@@ -0,0 +1,209 @@
+//
+//  ViewController.swift
+//  DisappearingUnicorns
+//
+//  Created by Arjun cosare on 2019-09-19.
+//  Copyright © 2019 Arjun cosare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var leaderboardButton: UIButton!
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var badButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    
+    var gameButtons = [UIButton]();
+    var gamePoints = 0;
+    var timer:Timer?
+    var currentButton:UIButton!
+    let defaults = UserDefaults.standard
+    var intervals:Float = 0.0
+    
+    enum GameState{
+        case gameOver
+        case playing
+    }
+        
+    var state = GameState.gameOver;
+    var playerInfo = PlayerData(name: "DefaultPlayer", points: 0, rank: 0,photo: UIImage(named: "person")!)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        pointsLabel.isHidden = true;
+        gameButtons = [goodButton, badButton];
+        setupFreshGameState();
+        configureDefaults();
+        
+        
+        defaults.addObserver(self, forKeyPath: "bgSwitchState", options: NSKeyValueObservingOptions.new, context: nil)
+        defaults.addObserver(self, forKeyPath: "colorSegmentIndex", options: NSKeyValueObservingOptions.new, context: nil)
+        defaults.addObserver(self, forKeyPath: "gameSpeed", options: NSKeyValueObservingOptions.new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        configureDefaults()
+    }
+    
+    //configurables
+    func configureDefaults(){
+        
+        let gameData = GameData()
+        
+        playerInfo = gameData.playerData(forName: defaults.string(forKey: "playerName") ?? "DefaultPlayer")
+        
+        if(defaults.bool(forKey: "bgSwitchState"))
+        {
+            view.backgroundColor = colorDeterminer(defaults.integer(forKey: "colorSegmentIndex"))
+        }
+        else
+        {
+            view.backgroundColor = UIColor.white
+        }
+        
+        intervals = defaults.float(forKey: "gameSpeed")
+        //if defaults are not set, set it to 0.1s
+        if(intervals == 0.0)	
+        {
+            intervals  = 0.1
+        }
+        
+    }
+    
+    func colorDeterminer(_ color: Int) -> UIColor{
+        switch color {
+        case 0:
+            return UIColor.red
+        case 1:
+            return UIColor.blue
+        case 2:
+            return UIColor.green
+        case 3:
+            return UIColor.yellow
+        default:
+            return UIColor.white
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //actions
+    @IBAction func startPressed(_ sender: Any) {
+        state = GameState.playing;
+        startNewGame();
+    }
+    
+    @IBAction func badPressed(_ sender: Any) {
+        badButton.isHidden = true;
+        timer?.invalidate();
+        gameOver();
+    }
+    
+    @IBAction func goodPressed(_ sender: Any) {
+        gamePoints = gamePoints + 1;
+        updatePointsLabel(gamePoints);
+        goodButton.isHidden = true;
+        timer?.invalidate();
+        oneGameRound();
+    }
+    
+    //Helpers
+    func startNewGame()
+    {
+        startGameButton.isHidden = true;
+        leaderboardButton.isHidden = true;
+        gamePoints = 0;
+        
+        updatePointsLabel(gamePoints);
+        pointsLabel.textColor = .magenta;
+        pointsLabel.isHidden = false;
+        oneGameRound();
+    }
+    
+    func oneGameRound()
+    {
+        updatePointsLabel(gamePoints);
+        displayRandomButton();
+        
+        timer = Timer.scheduledTimer(withTimeInterval: Double(intervals), repeats: false){ _ in
+            if self.state == GameState.playing
+            {
+                if self.currentButton == self.goodButton
+                {
+                    self.gameOver();
+                }
+                else
+                {
+                    self.oneGameRound();
+                }
+            }
+        }
+    }
+
+    func displayRandomButton(){
+        for myButton in gameButtons{
+            myButton.isHidden = true
+        }
+        let buttonIndex = Int.random(in: 0..<gameButtons.count)
+        currentButton = gameButtons[buttonIndex]
+        currentButton.center = CGPoint(x: randomXCoordinate(), y: randomYCoordinate())
+        currentButton.isHidden = false
+    }
+
+    func gameOver() {
+        state = GameState.gameOver
+        pointsLabel.textColor = .brown
+        setupFreshGameState()
+        
+        let gameData = GameData()
+        gameData.savePoints(gamePoints, for: playerInfo.name)
+    }
+
+    func setupFreshGameState() {
+        startGameButton.isHidden = false
+        leaderboardButton.isHidden = false
+        
+        for mybutton in gameButtons {
+            mybutton.isHidden = true
+        }
+        pointsLabel.alpha = 0.15
+        currentButton = goodButton
+        state = GameState.gameOver
+        
+    }
+    
+    func randCGFloat(_ min: CGFloat, _ max: CGFloat) -> CGFloat{
+        return CGFloat.random(in: min..<max)
+    }
+
+    func randomXCoordinate() -> CGFloat {
+        let left = view.safeAreaInsets.left + currentButton.bounds.width
+        let right = view.bounds.width - view.safeAreaInsets.right - currentButton.bounds.width
+        return randCGFloat(left, right)
+    }
+    
+    func randomYCoordinate() -> CGFloat {
+        let top = view.safeAreaInsets.top + currentButton.bounds.height
+        let bottom = view.bounds.height - view.safeAreaInsets.bottom - currentButton.bounds.height
+        return randCGFloat(top,bottom)
+    }
+    
+    func updatePointsLabel(_ newValue: Int) {
+        pointsLabel.text = "\(newValue)"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        if let detailsViewController = segue.destination as? SettingViewController{
+            detailsViewController.playerInfo = playerInfo
+        }
+    }
+}
+
